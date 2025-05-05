b6be7426f26408f22466d24e6ec134a27d74f1c2
jmsssc <sk353@uni-heidelberg.de>
2025-05-05 13:27:32 +0200

    Remove previously-removed imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Overview from './pages/Overview'
-import GlobalMapGrid from "./component/GlobalMapGrid.tsx";
-import GlobalMapCanvasOverlay from "./component/GlobalMapCanvasOverlay.tsx";
-import NutsMapComponent from "./component/NUTSMapper/NutsMapComponent.tsx";
-import NutsMapV2 from "./component/NUTSMapper/NutsMapV2.tsx";
-import RealNutsMapComponent from "./component/NUTSMapper/RealNutsMapComponent.tsx";
 import NutsMapV5 from "./component/NUTSMapper/NutsMapV5.tsx";
 
 function App() {
