81117bb2e9155474e0fea8faa17c9562ce69f5da
jmsssc <sk353@uni-heidelberg.de>
2025-05-28 14:19:34 +0200

    Add playwright testing and respective config/github action.

---
+++ frontend/tests/smoketest.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test('Frontend loads in browser', async ({ page }) => {
+  await page.goto('http://localhost:5174/');
+
+  await expect(page.locator('body')).toContainText('Health');
+});
