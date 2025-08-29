c244d58824763125c35bce520b5f1e716acfaae9
jmsssc <sk353@uni-heidelberg.de>
2025-08-29 13:35:55 +0200

    Remove dead code

--- frontend/tests/setup/apiMocks.ts
+++ frontend/tests/setup/apiMocks.ts
@@ -3,62 +3,6 @@ import type { Page } from "@playwright/test";
 const MOCK_2016_FILE_PATH = "/tests/setup/MockResponse2258.json";
 const MOCK_2017_FILE_PATH = "/tests/setup/MockResponse4823.json";
 
-interface MockResponse {
-	result: {
-		"latitude, longitude, var_value": number[][];
-	};
-}
-
-export async function setupApiMocks(page: Page) {
-	const mockData = (await import(MOCK_2017_FILE_PATH)) as MockResponse;
-
-	await page.route(
-		"**/api/cartesian?requested_time_point=2016-07-01&requested_variable_value=R0",
-		async (route) => {
-			try {
-				const mockData = (await import(MOCK_2016_FILE_PATH)) as MockResponse;
-				await route.fulfill({
-					status: 200,
-					contentType: "application/json",
-					body: JSON.stringify(mockData),
-				});
-			} catch (error) {
-				console.warn("Failed to load mock data for 2016:", error);
-				await route.continue();
-			}
-		},
-	);
-
-	const returnFakeAlternateData = async (route) => {
-		try {
-			await route.fulfill({
-				status: 200,
-				contentType: "application/json",
-				body: JSON.stringify(mockData),
-			});
-		} catch (error) {
-			console.warn("Failed to load mock data for 2017:", error);
-			await route.continue();
-		}
-	};
-
-	await page.route(
-		"**/api/cartesian?requested_time_point=2016-08-01&requested_variable_value=R0",
-		async (route) => {
-			console.log("2016-08 route hit.");
-			return await returnFakeAlternateData(route);
-		},
-	);
-
-	// Mock the alternative data endpoint, this is either 2017 or the 8th month (august) instead of July for the month test.
-	await page.route(
-		"**/api/cartesian?requested_time_point=2017-07-01&requested_variable_value=R0",
-		async (route) => {
-			return await returnFakeAlternateData(route);
-		},
-	);
-}
-
 // Helper function to create route handler that loads mock data
 async function createMockHandlerForYear(mockFilePath: string) {
 	console.log("Now using normal mocks");
